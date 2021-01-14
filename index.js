2dcdb0216e8f681582948a99e7990bb59d1f966f
Katrina Pritchard <pritckat@gmail.com>
2021-01-14 17:09:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,10 +19,4 @@ const fareDoubler = createFareMultiplier(2);
 
 const fareTripler = createFareMultiplier(3);
 
-function fetchSpecifiedDrivers(drivers, cb) {
-  return cb(drivers)
-<<<<<<< HEAD
-}
-=======
-}
->>>>>>> e814acb81c03d1a0552e24f95593664a8515d684
+
